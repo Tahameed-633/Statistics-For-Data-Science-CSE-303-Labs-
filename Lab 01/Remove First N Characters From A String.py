2bc51a708a690ee3bf4c6049fbf657fd9a1cbd54
Tahameed-633 <tahameedkarim@gmail.com>
2026-08-02 16:07:57 +0600

    Update Remove First N Characters From A String.py

--- Lab 01/Remove First N Characters From A String.py
+++ Lab 01/Remove First N Characters From A String.py
@@ -1,1 +1,12 @@
-
+str1=input("Enter the string:")
+n=int(input("Enter the value of n:"))
+l=0
+for ch in str1:
+    l=l+1
+if(n>l or n<=0):
+    print("Invalid input!")
+else:
+    str2=""
+    for i in range(n,l):
+        str2=str2+str1[i]
+print(str2)
