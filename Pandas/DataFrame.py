1cea5b0625cf9e3195c53611d21d1f745bdd13c9
Tahameed Karim <tahameedkarim@gmail.com>
2026-08-05 09:00:27 +0000

    DataFrame creation

---
+++ Pandas/DataFrame.py
@@ -0,0 +1,9 @@
+import pandas as pd
+data={
+    "Name":["Safin","Fatin","Irin","Tasnim"],
+    "Department":["CSE","ME","EEE","NAME"],
+    "CGPA":[3.06,3.24,3.50,3.35]
+
+}
+df=pd.DataFrame(data)
+print(df)
