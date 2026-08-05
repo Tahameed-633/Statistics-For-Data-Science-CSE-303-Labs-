647e310a41d9e01aca50224f074eaa0008372fbf
Tahameed Karim <tahameedkarim@gmail.com>
2026-08-05 07:16:19 +0000

    pasdas series

--- Pandas/Pandas Series.py
+++ Pandas/Pandas Series.py
@@ -1,1 +1,11 @@
+import pandas as pd
+obj=pd.Series([4,7,-5,3])
+import pandas as pd
+obj=pd.Series([4,7,-5,3])
+print("Original series:")
+print(obj)
+print("Even series:")
+print(obj[obj%2==0])
+
+
 
